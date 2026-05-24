d686e8320d995a73b2cc1127ab690724fba20179
Pratik Gaikwad <gaikwadpratik8105@gmail.com>
2026-05-24 20:53:02 +0530

    Fixed static files with WhiteNoise

--- cloudPulse/settings.py
+++ cloudPulse/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
 'django.middleware.security.SecurityMiddleware',
+'whitenoise.middleware.WhiteNoiseMiddleware',
 'django.contrib.sessions.middleware.SessionMiddleware',
 'django.middleware.common.CommonMiddleware',
 'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,10 +124,14 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-BASE_DIR / "advisor/static",
+    BASE_DIR / "advisor/static",
 ]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = (
+    "whitenoise.storage.CompressedManifestStaticFilesStorage"
+)
 
 # Default primary key
 
